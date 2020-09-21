ce5d4babcf66efad3eff20d593d18d4d7c13fede
Tiago Mello <tiago.gomes.sistemas@gmail.com>
2020-09-21 08:53:25 -0300

    Criacao da classe SequenciaCrescente.php

---
+++ SequenciaCrescente.php
@@ -0,0 +1,13 @@
+<?php
+    function SequenciaCrescente ($sequencia)
+    {
+        $sequenciaNumerica = rsort ($sequencia);
+        $negacao = 0;
+        $contador = count($sequencia);
+        for($controle = 0; $controle <  $sequencia; $controle ++){
+            if($sequenciaNumerica[$controle] >= $sequenciaNumerica[$controle +1]){$negacao ++}
+        }
+        if($negacao > 1){return false;} else {return true}
+    }
+?>
+
