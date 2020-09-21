c6188ab209a722dc71b80cf1618e6ecc3560281e
Tiago Mello <tiago.gomes.sistemas@gmail.com>
2020-09-21 09:04:08 -0300

    Correcao do if dentro do for em SequenciaCrescente.php

--- SequenciaCrescente.php
+++ SequenciaCrescente.php
@@ -5,7 +5,7 @@
         $negacao = 0;
         $contador = count($sequencia);
         for($controle = 0; $controle <  $sequencia; $controle ++){
-            if($sequenciaNumerica[$controle] >= $sequenciaNumerica[$controle +1]){$negacao ++}
+            if($sequenciaNumerica[$controle] == $sequenciaNumerica[$controle +1]){$negacao ++}
         }
         if($negacao > 1){return false;} else {return true}
     }
